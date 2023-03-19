54df7b5b3a03bda2ee6f45b3dbefaeb0d58b1efa
Huymobile <huy.tran@kyanon.digital>
2023-03-20 01:03:01 +0700

    ⚡️ update new feed

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,14 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI, {
-            useUnifiedTopology: true,
-            useNewUrlParser: true
-        });
-    } catch (err) {
-        console.error(err);
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
 
-module.exports = connectDB
+    await mongoose.connect(process.env.DATABASE_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+module.exports = connectDB;
